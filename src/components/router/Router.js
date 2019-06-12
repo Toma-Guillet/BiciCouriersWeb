94ae1937bf04421270845bbadc182669e3ed25a8
Thomas <thomas.guillet35@hotmail.fr>
2019-06-12 11:51:44 +0200

    add route :bug:

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../../screens/home/Home';
+import Home from '../../screens/Home';
+import Map from '../../screens/Map';
+import Contacts from '../../screens/Contacts';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/map" exact component={Map} />
+        <Route path="/contacts" exact component={Contacts} />
         <Route path="*" exact component={Home} />
       </Switch>
     </BrowserRouter>

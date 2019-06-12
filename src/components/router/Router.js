ce1dbdb29f4cb69ed35760adbfda37bb55b9050a
guillaume <guillaume-duclos@hotmail.fr>
2019-06-12 11:33:39 +0200

    Add react-dnd and add BrowserRouter

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../../screens/home/Home';
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="*" exact component={Home} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   )
 };
 

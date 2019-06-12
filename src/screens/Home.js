43f5f902531acced8ab30a368ea43045c4b29937
Thomas <thomas.guillet35@hotmail.fr>
2019-06-12 17:47:45 +0200

    WIP Drag & Drop :twisted_rightwards_arrows:

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import MenuNavigation from '../components/menuNavigation/MenuNavigation.js';
+import DndList from '../components/dndList/dndList.js';
 
 import '../styles/common.scss';
 
+
+
 class Home extends Component {
+
   render() {
     return (
       <div className="home">
-        Liste des commandes
+        <DndList />
         <MenuNavigation />
       </div>
     );

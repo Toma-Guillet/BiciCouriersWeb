3b8ba87cec2ce0b8cda862beccf18e6fa3139a8f
guillaume <guillaume-duclos@hotmail.fr>
2019-06-18 09:51:02 +0200

    Map

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -10,10 +10,10 @@ class Map extends Component {
   changeMode = value => {
     switch (value) {
       case 'couriers':
-        this.setState({login: value});
+        this.setState({mode: 'couriers'});
         break;
       case 'livraisons':
-        this.setState({password: value});
+        this.setState({mode: 'livraisons'});
         break;
       default:
         return;
@@ -21,12 +21,12 @@ class Map extends Component {
   };
 
   render() {
-    console.log(this.state);
+    console.log(this.state.mode);
     return (
       <div className="mapScreen">
         <header>
-          <button onClick={() => this.changeMode('couriers')} className="pointer buttonActive">Coursiers</button>
-          <button onClick={() => this.changeMode('livraisons')} className="pointer">Livraisons</button>
+          <button onClick={() => this.changeMode('couriers')} className={`${this.state.mode = 'couriers' ? 'buttonActive' : 'buttonInactive'} pointer`}>Coursiers</button>
+          <button onClick={() => this.changeMode('livraisons')} className={`${this.state.mode = 'livraisons' ? 'buttonActive' : 'buttonInactive'} pointer`}>Livraisons</button>
         </header>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d43362.70604045212!2d-1.6055301208984372!3d47.21327295000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1560762539142!5m2!1sfr!2sfr"

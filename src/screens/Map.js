354e0e090971d63401e14f40ed9dcfb1fbaaf36f
guillaume <guillaume-duclos@hotmail.fr>
2019-06-20 11:33:52 +0200

    Map

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -3,22 +3,24 @@ import Navigation from '../components/menuNavigation/MenuNavigation.js';
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import googleMaps from '../configs/googleMaps';
+import mapOptions from '../configs/mapOptions';
 
 const MapComponent = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${googleMaps.apiKey}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div className="mapComponent" />,
-    mapElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
 )(props =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={14}
+    defaultCenter={{ lat: 47.2145519, lng: -1.5652564 }}
+    options={mapOptions}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    {props.isMarkerShown && <Marker position={{ lat: 47.2145519, lng: -1.5652564 }} onClick={props.onMarkerClick} />}
   </GoogleMap>
 );
 
@@ -27,6 +29,7 @@ class Map extends Component {
   state = {
     mode: 'couriers',
     isMarkerShown: false,
+    markerInfosHeight: 0
   };
 
   changeMode = value => {
@@ -43,22 +46,22 @@ class Map extends Component {
   };
 
   componentDidMount() {
-    this.delayedShowMarker()
+    this.setState({
+      isMarkerShown: true,
+      markerInfosHeight: this.refs.markerInfos.clientHeight
+    });
   }
 
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
+  onMarkerClick = () => {
+    this.setState({markerInfosHeight: 0});
   };
 
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false });
-    this.delayedShowMarker()
+  closeMarkerInfos = () => {
+    this.setState({markerInfosHeight: this.refs.markerInfos.clientHeight});
   };
 
   render() {
-    console.log(this.state.mode);
+    console.log(this.state.markerInfosHeight);
     return (
       <div className="mapScreen">
         <header>
@@ -67,9 +70,29 @@ class Map extends Component {
         </header>
         <MapComponent
           isMarkerShown={this.state.isMarkerShown}
-          onMarkerClick={this.handleMarkerClick}
+          onMarkerClick={this.onMarkerClick}
         />
-        <Navigation />
+        <Navigation/>
+        <div className="markerInfos" ref="markerInfos" style={{bottom: `-${this.state.markerInfosHeight}px`}}>
+          <button className="exitButton pointer" onClick={this.closeMarkerInfos}></button>
+          <p className="title upper">En cours de livraison</p>
+          <div className="infos">
+            <div>
+              <ul>
+                <li>Agence Géniale</li>
+                <li>Rue des Lilas<br/>44 000 Nantes</li>
+                <li>06 80 50 40 40</li>
+              </ul>
+            </div>
+            <div>
+              <p>10:00 > 12:00</p>
+            </div>
+            <div>
+              <p>Merci de contacter Mr. Martin lorsque vous arrivez sur place. Son numéro est 06 60 05 06 06. Le colis est fragile.</p>
+            </div>
+          </div>
+          <button className="upper pointer">Ouvrir la commande</button>
+        </div>
       </div>
     );
   }

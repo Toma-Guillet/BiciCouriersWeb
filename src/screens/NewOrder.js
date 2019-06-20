390863b7e0ca5b1c491586f9b539f79206aa8aa4
guillaume <guillaume-duclos@hotmail.fr>
2019-06-20 16:43:42 +0200

    Page new

--- src/screens/NewOrder.js
+++ src/screens/NewOrder.js
@@ -7,9 +7,47 @@ import '../styles/common.scss';
 class NewOrder extends Component {
   render() {
     return (
-      <div className="home">
-        <Navigation />
+      <div className="new">
         <MenuTools />
+        <header>
+          <ul>
+            <li>
+              <p className="upper center">Client</p>
+              <p className="center">1</p>
+            </li>
+            <li>
+              <p className="upper center">Horaires</p>
+              <p className="center">2</p>
+            </li>
+            <li>
+              <p className="upper center">Adresses</p>
+              <p className="center">3</p>
+            </li>
+          </ul>
+        </header>
+        <main>
+          <form>
+            <h2>Type</h2>
+            <div className="type">
+              <div className="checkbox pointer">
+                <input type="checkbox" id="scales" name="scales"/>
+                <label className="pointer" for="scales">Professionel</label>
+              </div>
+              <div className="checkbox pointer">
+                <input type="checkbox" id="scales" name="scales"/>
+                <label className="pointer" for="scales">Particulier</label>
+              </div>
+            </div>
+            <h2>Identité client</h2>
+            <div className="identity">
+              <input type="text" placeholder="Nom du client"/>
+              <label>Cette société est un nouveau client et va être ajoutée à la base de donnée</label>
+            </div>
+            <input type="submit" value="SUIVANT"/>
+            <input type="cancel" value="ANNULER" className="pointer center"/>
+          </form>
+        </main>
+        <Navigation />
       </div>
     );
   }

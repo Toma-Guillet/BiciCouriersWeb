b0cff1c44f9721c81714c07dc2fc518d9eb40afa
guillaume <guillaume-duclos@hotmail.fr>
2019-06-24 16:42:16 +0200

    New order

--- src/screens/NewOrder.js
+++ src/screens/NewOrder.js
@@ -5,28 +5,51 @@ import MenuTools from '../components/MenuTools/MenuTools';
 import '../styles/common.scss';
 
 class NewOrder extends Component {
+
+  state = {
+    step: 1
+  };
+
+  changeStep = (step, event) => {
+    event.preventDefault();
+    switch (step) {
+      case 1:
+        this.setState({step: 2});
+        break;
+      case 2:
+        this.setState({step: 3});
+        break;
+      case 3:
+        this.setState({step: 3});
+        break;
+      default:
+        return;
+    }
+  };
+
   render() {
     return (
       <div className="new">
         <MenuTools />
         <header>
           <ul>
-            <li>
+            <li className="activeStep">
               <p className="upper center">Client</p>
               <p className="center">1</p>
             </li>
-            <li>
+            <li className={this.state.step === 2 || this.state.step === 3 ? 'activeStep' : 'inactiveStep'}>
               <p className="upper center">Horaires</p>
               <p className="center">2</p>
             </li>
-            <li>
+            <li className={this.state.step === 3 ? 'activeStep' : 'inactiveStep'}>
               <p className="upper center">Adresses</p>
               <p className="center">3</p>
             </li>
           </ul>
         </header>
         <main>
-          <form>
+
+          <form className={this.state.step === 1 ? 'visible' : 'hidden'} onSubmit={event => this.changeStep(1, event)}>
             <h2>Type</h2>
             <div className="type">
               <div className="checkbox pointer">
@@ -43,9 +66,91 @@ class NewOrder extends Component {
               <input type="text" placeholder="Nom du client"/>
               <label>Cette société est un nouveau client et va être ajoutée à la base de donnée</label>
             </div>
-            <input type="submit" value="SUIVANT"/>
-            <input type="cancel" value="ANNULER" className="pointer center"/>
+            <input type="submit" value="Suivant" className="upper"/>
+            <input type="cancel" value="Annuler" className="upper pointer center"/>
           </form>
+
+          <form className={this.state.step === 2 ? 'visible' : 'hidden'} onSubmit={event => this.changeStep(2, event)}>
+            <h2>Mode de livraison</h2>
+            <div className="livraison">
+              <input type="checkbox" placeholder="Nom du client"/>
+              <label className="center pointer">Dans la journée</label>
+            </div>
+            <div className="livraison">
+              <input type="checkbox" placeholder="Nom du client"/>
+              <label className="center pointer">Moins de 45 minutes</label>
+            </div>
+            <div className="livraison">
+              <input type="checkbox" placeholder="Nom du client"/>
+              <label className="center pointer">Définir une plage horaire</label>
+            </div>
+            <div className="livraison">
+              <input type="checkbox" placeholder="Nom du client"/>
+              <label className="center pointer">Location</label>
+            </div>
+            <h2>Volume du colis</h2>
+            <div className="volume">
+              <div className="checkbox pointer">
+                <input type="checkbox" id="scales" name="scales"/>
+                <label className="pointer center" htmlFor="scales">Normal pour vélo</label>
+              </div>
+              <div className="checkbox pointer">
+                <input type="checkbox" id="scales" name="scales"/>
+                <label className="pointer center" htmlFor="scales">Volumineux pour cargo</label>
+              </div>
+            </div>
+            <h2>Réductions</h2>
+            <div className="pointer checkboxBetween">
+              <input type="checkbox" id="scales" name="scales"/>
+              <label className="pointer" htmlFor="scales">Course offerte</label>
+            </div>
+            <div className="pointer checkboxBetween">
+              <input type="checkbox" id="scales" name="scales"/>
+              <label className="pointer" htmlFor="scales">Ajustement tickets</label>
+            </div>
+
+            <input type="submit" value="Suivant" className="upper"/>
+            <input type="cancel" value="Annuler" className="upper pointer center"/>
+          </form>
+
+          <form className={this.state.step === 3 ? 'visible' : 'hidden'} onSubmit={event => this.changeStep(3, event)}>
+            <h1>Pickup</h1>
+
+            <h2>Date</h2>
+            <input type="date"/>
+
+            <h2>Horaires</h2>
+            <input type="time"/>
+
+            <h2>Récurrence</h2>
+            <select className="selection">
+              <option value="volvo">Pas de récurrence</option>
+              <option value="saab">Récurrence</option>
+            </select>
+
+            <h2>Commentaire</h2>
+            <textarea/>
+
+            <h1>Drop</h1>
+
+            <h2>Date</h2>
+            <input type="date"/>
+
+            <h2>Horaires</h2>
+            <input type="time"/>
+
+            <h2>Récurrence</h2>
+            <select className="selection">
+              <option value="volvo">Pas de récurrence</option>
+              <option value="saab">Récurrence</option>
+            </select>
+
+            <h2>Commentaire</h2>
+            <textarea/>
+
+            <input type="submit" value="Valider" className="upper"/>
+          </form>
+
         </main>
         <Navigation />
       </div>

0ffd890dabc8d681d546902a97ff9c1dc665de8f
guillaume <guillaume-duclos@hotmail.fr>
2019-06-25 13:18:36 +0200

    update

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,12 +5,12 @@ import Column from '../Column/Column';
 
 const initialData = {
     tasks: {
-      'task-1': { id: 'task-1', content: 'Biocop' },
-      'task-2': { id: 'task-2', content: 'Fleuriste' },
-      'task-3': { id: 'task-3', content: 'Fleuriste' },
-      'task-4': { id: 'task-4', content: 'Fleuriste' },
-      'task-5': { id: 'task-5', content: 'Fleuriste' },
-      'task-6': { id: 'task-6', content: 'Fleuriste' },
+      'task-1': { id: 'task-1', content: 'Biocop', adresse: '9 rue de la Verrerie', number: 1, type: 'Pick Up', time: '09:00 > 12:00' },
+      'task-2': { id: 'task-2', content: 'Fleuriste', adresse: '11 rue des Olivettes', number: 2, type: 'Drop', time: '10:00 > 18:00', urgent: true },
+      'task-3': { id: 'task-3', content: 'Pharmacie', adresse: '10 avenue des Lilas', number: 3, type: 'Drop', time: '09:15 > 13:00' },
+      'task-4': { id: 'task-4', content: 'La bonne franquette', adresse: '2 rue Léon Say', number: 4, type: 'Pick Up', time: '17:30 > 19:00' },
+      'task-5': { id: 'task-5', content: 'Honoré', adresse: '2 rue du calvaire', number: 5, type: 'Drop', time: '09:00 > 12:00', urgent: true },
+      'task-6': { id: 'task-6', content: 'FreshSushi', adresse: '9 rue de la fourche', number: 6, type: 'Pick Up', time: '07:00 > 09:00' }
     },
     columns: {
       'column-1': {

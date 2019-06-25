f8cbebf68e59503fd1a973ecc3b970ae26a6bcbd
Thomas <thomas.guillet35@hotmail.fr>
2019-06-25 11:48:45 +0200

    Updates

---
+++ precache-manifest.13d213d501e1484f374fc7b9d4779b2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11304ce85fd9dae6619b9601e9e4b16",
+    "url": "/BiciCouriersWeb/index.html"
+  },
+  {
+    "revision": "49b64b3129d90261848e",
+    "url": "/BiciCouriersWeb/static/css/main.cd2ef2cf.chunk.css"
+  },
+  {
+    "revision": "70ce11460d04d197bc56",
+    "url": "/BiciCouriersWeb/static/js/2.b9eb4ebb.chunk.js"
+  },
+  {
+    "revision": "49b64b3129d90261848e",
+    "url": "/BiciCouriersWeb/static/js/main.2d5735e3.chunk.js"
+  },
+  {
+    "revision": "3eaa1e136e0d9dcf6839",
+    "url": "/BiciCouriersWeb/static/js/runtime~main.23d1a68c.js"
+  },
+  {
+    "revision": "69b92f7ec0bf8a69a50bab7295b2a3f9",
+    "url": "/BiciCouriersWeb/static/media/Karla-Regular.69b92f7e.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/BiciCouriersWeb/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "ee8bb6b2ed61a800d4a028182d456233",
+    "url": "/BiciCouriersWeb/static/media/gaspard.ee8bb6b2.png"
+  },
+  {
+    "revision": "6c5883e7ef8f6e79a197fe1b6f0a8b35",
+    "url": "/BiciCouriersWeb/static/media/logo.6c5883e7.png"
+  }
+]);
